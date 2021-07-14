2b0a9d1648e8471f2f4b4bc704fbdbaeaf99b45c
ReezanVisram <59345266+ReezanVisram@users.noreply.github.com>
2021-07-14 14:49:15 -0400

    Version 1.0.0

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Footer.css';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 
 export default function Footer() {
@@ -9,8 +10,9 @@ export default function Footer() {
         <div className={'footer-container'}>
                <h3>Reezan Visram</h3>
                <div className={'footer-icon-container'}>
-                   <a href='https://instagram.com/reezanvisram' target='_blank' rel='noreferrer'><InstagramIcon style={{fontSize: '2rem'}} /></a>
+                   <a href='https://linkedin.com/in/reezan-visram' target='_blank' rel='noreferrer'><LinkedInIcon style={{fontSize: '2rem'}}/></a>
                    <a href='https://github.com/reezanvisram' target='_blank' rel='noreferrer'><GitHubIcon style={{fontSize: '2rem'}} /></a>
+                   <a href='https://instagram.com/reezanvisram' target='_blank' rel='noreferrer'><InstagramIcon style={{fontSize: '2rem'}} /></a>
                </div>
         </div>
     )
