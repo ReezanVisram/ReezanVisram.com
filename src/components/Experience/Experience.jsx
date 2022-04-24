36607f422012b626dcd3275f36ac404683967fe7
Reezan Visram <rhvisram@uwaterloo.ca>
2022-04-24 18:28:25 -0400

    update site with experience, projects and resume from co-op term

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Experience.css';
-import shortid from "shortid";
+import shortid from 'shortid';
 
 import SectionHeading from '../util/SectionHeading/SectionHeading';
 
@@ -8,6 +8,25 @@ export default function Experience() {
     const jobs = [
         {
             id: 0,
+            company: 'Red Thread Innovations',
+            title: 'Software Engineering Intern (Co-op)',
+            time: 'January 2022 - April 2022',
+            points: [
+                'Developed a full-stack application management system and multiple microservices using React, Express, TypeScript and PostgreSQL, used by over 10 companies',
+                'Wrote automated test suites to achieve 99% code coverage using Jest and React-Testing-Library',
+                'Containerized applications and microservices using Docker to deploy to AWS EC2 environments',
+                'Worked in an Agile team framework, and participated in and led various ceremonies, including 2-week sprints, sprint review, sprint retrospective and daily stand-ups',
+                'Initiated and led discussions about improving workflow and increasing efficiency, leading to increased velocity and quality of deliverables',
+            ],
+            technologies: [
+                'TypeScript (React, Express)',
+                'PostgreSQL (TypeORM)',
+                'Docker',
+                'Jest',
+            ],
+        },
+        {
+            id: 1,
             company: 'Hack the Northeast',
             title: 'Frontend Developer',
             time: 'August 2020 - June 2021',
@@ -17,36 +36,36 @@ export default function Experience() {
                 'Created functional and informative components for over 1000 prospective participants to learn about and sign up for events',
                 'Worked with graphic designers and provided recommendations to design teams to streamline and enhance the website',
                 'Tested and fixed all components of the website before deploying to GitHub',
-                'Used responsive design principles and technologies to ensure the website looked stunning on all screen sizes'
+                'Used responsive design principles and technologies to ensure the website looked stunning on all screen sizes',
             ],
             technologies: [
                 'ReactJS',
                 'Material UI',
                 'GSAP.js',
                 'Git',
-                'Github'
-            ]
+                'Github',
+            ],
         },
 
         {
-            id: 1,
+            id: 2,
             company: 'Dunbarton High School',
             title: 'Math Tutor',
             time: 'January 2018 - March 2020',
             points: [
                 'Conducted one-on-one learning sessions for Mathematics',
                 'Evaluated student performance and provided academic counselling as needed',
-                'Selected as a “Peer Tutor” to aid struggling grade 9 students build the foundations necessary to succeed in Mathematics courses throughout high school and beyond'
-            ]
-        }
-    ]
-    
+                'Selected as a “Peer Tutor” to aid struggling grade 9 students build the foundations necessary to succeed in Mathematics courses throughout high school and beyond',
+            ],
+        },
+    ];
+
     const [activeJob, setActiveJob] = React.useState(jobs[0]);
 
     const getId = () => {
         const id = shortid.generate();
         return id;
-    }
+    };
 
     return (
         <div className={'experience-container'}>
@@ -55,36 +74,58 @@ export default function Experience() {
             <div className={'experience-content-container'}>
                 <div className={'job-button-container'}>
                     {jobs.map((job) => {
-                        if (activeJob.company === job.company && activeJob.title === job.title) {
-                            return <button className={'job-button active'} onClick={() => setActiveJob(jobs[job.id])}>{job.company}</button>
+                        if (
+                            activeJob.company === job.company &&
+                            activeJob.title === job.title
+                        ) {
+                            return (
+                                <button
+                                    className={'job-button active'}
+                                    onClick={() => setActiveJob(jobs[job.id])}
+                                >
+                                    {job.company}
+                                </button>
+                            );
                         } else {
-                            return <button className={'job-button'} onClick={() => setActiveJob(jobs[job.id])}>{job.company}</button>
+                            return (
+                                <button
+                                    className={'job-button'}
+                                    onClick={() => setActiveJob(jobs[job.id])}
+                                >
+                                    {job.company}
+                                </button>
+                            );
                         }
-                        
                     })}
                 </div>
 
                 <div key={getId()} className={'active-job-information'}>
                     <div className={'active-job-heading'}>
-                        <h4>{activeJob.title} @ {activeJob.company}</h4>
+                        <h4>
+                            {activeJob.title} @ {activeJob.company}
+                        </h4>
                         <p>{activeJob.time}</p>
                     </div>
-                    
+
                     <div className={'active-job-description'}>
                         <ul className={'active-job-points-list'}>
                             {activeJob.points.map((point) => {
-                                return <li>{point} <br /></li>
+                                return (
+                                    <li>
+                                        {point} <br />
+                                    </li>
+                                );
                             })}
                         </ul>
-                        {activeJob.technologies &&
-                            <h5>Technologies: {activeJob.technologies.join(', ')}</h5>
-                        }
+                        {activeJob.technologies && (
+                            <h5>
+                                Technologies:{' '}
+                                {activeJob.technologies.join(', ')}
+                            </h5>
+                        )}
                     </div>
                 </div>
-
             </div>
-
-
         </div>
-    )
+    );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,22 +8,62 @@ import OtherProject from './OtherProject/OtherProject';
 import NHLSimulation from '../../images/Projects/NHLSimulation.png';
 import ImproveMint from '../../images/Projects/ImproveMint.png';
 import PillsOnWheels from '../../images/Projects/PillsOnWheels.png';
+import TypeR from '../../images/Projects/TypeR.png';
+import FractalVisualizer from '../../images/Projects/FractalVisualizer.png';
+import MorseCodeTeachingDevice from '../../images/Projects/MorseCodeTeachingDevice.png';
 
 export default function Projects() {
-
     const featuredProjects = [
+        {
+            name: 'TypeR',
+            img: TypeR,
+            type: 'web',
+            technologies: [
+                'TypeScript (React, Express)',
+                'MySQL (TypeORM)',
+                'Docker',
+                'Python (requests, pony ORM)',
+            ],
+            summary:
+                'A hub for custom mechanical keyboard components. Data scraped daily from many major component retailers and compiled into one site.',
+            githubLink: 'https://github.com/reezanvisram/TypeR',
+            releaseLink: 'https://github.com/reezanvisram/TypeR',
+        },
+        {
+            name: 'FractalVisualizer',
+            img: FractalVisualizer,
+            type: 'download',
+            technologies: ['C++', 'OpenGL', 'Dear ImGui'],
+            summary:
+                'A C++ and OpenGL program to render customizable and interactive fractals, such as the Mandelbrot Set and Heighway Dragon.',
+            githubLink: 'https://github.com/reezanvisram/fractalvisualizer',
+            releaseLink: process.env.PUBLIC_URL + '/FractalVisualizer.zip',
+        },
+        {
+            name: 'Morse Code Teaching Device',
+            img: MorseCodeTeachingDevice,
+            type: 'web',
+            technologies: [
+                'C',
+                'PlatformIO',
+                'STM32 Nucleo F410RE Microcontroller',
+            ],
+            summary:
+                'An electronic device that teaches the user the basics of Morse code (reading, listening, speaking) through constant repetition',
+            githubLink:
+                'https://github.com/reezanvisram/morse-code-teaching-device',
+            releaseLink:
+                'https://github.com/reezanvisram/morse-code-teaching-device',
+        },
         {
             name: 'NHL Simulation',
             img: NHLSimulation,
             type: 'download',
-            technologies: [
-                'Python',
-                'kivy',
-                'requests'
-            ],
-            summary: 'A desktop application that takes the stats of every NHL Player and uses them to create a realistic simulation of an NHL season',
+            technologies: ['Python', 'kivy', 'requests'],
+            summary:
+                'A desktop application that takes the stats of every NHL Player and uses them to create a realistic simulation of an NHL season',
             githubLink: 'https://github.com/reezanvisram/NHLSimulation',
-            releaseLink: process.env.PUBLIC_URL + "/NHLSimulation.zip"
+            releaseLink: process.env.PUBLIC_URL + '/NHLSimulation.zip',
         },
         {
             name: 'ImproveMint',
@@ -34,81 +74,74 @@ export default function Projects() {
                 'CSS',
                 'JavaScript',
                 'Python (Flask)',
-                'MySQL'
+                'MySQL',
             ],
-            summary: 'A self-improvement website where users can trask tasks and habits that they want to build',
+            summary:
+                'A self-improvement website where users can trask tasks and habits that they want to build',
             githubLink: 'https://github.com/reezanvisram/improvemint',
-            releaseLink: 'http://www.reezanvisram.com/improvemint'
+            releaseLink: 'http://www.reezanvisram.com/improvemint',
         },
         {
             name: 'PillsOnWheels',
             img: PillsOnWheels,
             type: 'web',
-            technologies: [
-                'React Native',
-                'Python (Flask)',
-                'SQLite'
-            ],
-            summary: 'An Android app for users to manage presecriptions, as well as order them for delivery. It is deployed on the Google Play Store',
+            technologies: ['React Native', 'Python (Flask)', 'SQLite'],
+            summary:
+                'An Android app for users to manage presecriptions, as well as order them for delivery. It is deployed on the Google Play Store',
             githubLink: 'https://github.com/reezanvisram/PillsOnWheels',
-            releaseLink: 'https://play.google.com/store/apps/details?id=com.Visram_INC.PillsOnWheels'
-        }
-    ]
+            releaseLink:
+                'https://play.google.com/store/apps/details?id=com.Visram_INC.PillsOnWheels',
+        },
+    ];
 
     const otherProjects = [
         {
             name: 'Pharmaceutica',
             type: 'download',
-            technologies: [
-                'C#',
-                'Unity Game Engine'
-            ],
-            summary: 'A tycoon game where a user takes control of a pharmaceutical company racing to develop a vaccine for COVID-19',
+            technologies: ['C#', 'Unity Game Engine'],
+            summary:
+                'A tycoon game where a user takes control of a pharmaceutical company racing to develop a vaccine for COVID-19',
             githubLink: 'https://github.com/reezanvisram/pharmaceutica',
-            releaseLink: process.env.PUBLIC_URL + '/Pharmaceutica.zip'
+            releaseLink: process.env.PUBLIC_URL + '/Pharmaceutica.zip',
         },
         {
             name: 'Complete Crisis Coverage',
             type: 'web',
-            technologies: [
-                'HTML',
-                'Bootstrap',
-                'Flask',
-                'MySQL'
-            ],
-            summary: 'A website where users can see COVID-19 Statistics from all over the world, as well as read firsthand stories of how COVID-19 has affected people globally',
-            githubLink: 'https://github.com/reezanvisram/completecrisiscoverage',
-            releaseLink: 'http://www.reezanvisram.com/completecrisiscoverage'
+            technologies: ['HTML', 'Bootstrap', 'Flask', 'MySQL'],
+            summary:
+                'A website where users can see COVID-19 Statistics from all over the world, as well as read firsthand stories of how COVID-19 has affected people globally',
+            githubLink:
+                'https://github.com/reezanvisram/completecrisiscoverage',
+            releaseLink: 'http://www.reezanvisram.com/completecrisiscoverage',
         },
         {
             name: 'Arduino Information Display',
             type: 'web',
-            technologies: [
-                'Arduino',
-                'C++'
-            ],
-            summary: 'A heads up display that shows the date, time, humidity and temperature, controlled using an IR remote',
-            githubLink: 'https://github.com/reezanvisram/arduinoinformationdisplay',
-            releaseLink: 'https://github.com/reezanvisram/arduinoinformationdisplay'
-        }
-    ]
-
+            technologies: ['Arduino', 'C++'],
+            summary:
+                'A heads up display that shows the date, time, humidity and temperature, controlled using an IR remote',
+            githubLink:
+                'https://github.com/reezanvisram/arduinoinformationdisplay',
+            releaseLink:
+                'https://github.com/reezanvisram/arduinoinformationdisplay',
+        },
+    ];
 
     return (
         <div className={'projects-container'}>
             <SectionHeading heading={'Projects'} />
-        
-            <div className="featured-projects-content-container">
+
+            <div className='featured-projects-content-container'>
                 {featuredProjects.map((project) => {
-                    return <FeaturedProject project={project} />
+                    return <FeaturedProject project={project} />;
                 })}
             </div>
 
-            <div className="other-projects-content-container">
+            <div className='other-projects-content-container'>
                 {otherProjects.map((project) => {
-                    return <OtherProject project={project} />
+                    return <OtherProject project={project} />;
                 })}
             </div>
         </div>
-    )
+    );
 }
